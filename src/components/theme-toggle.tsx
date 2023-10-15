dfc28ba8cddebaf7e71f49373269dd1e7ae8061d
edward <bepyan@naver.com>
2023-10-15 23:43:42 +0900

    ✧ darkmode

---
+++ src/components/theme-toggle.tsx
@@ -0,0 +1,78 @@
+import { useEffect, useState } from 'react';
+
+import { Button } from '~/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '~/components/ui/dropdown-menu';
+
+import { MoonIcon, SunIcon } from './ui/icons';
+
+export default function ThemeToggle() {
+  const [theme, setTheme] = useState<string>();
+
+  const listenDarkMode = () => {
+    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+    const handle = (query: { matches: boolean }) => {
+      if (query.matches) {
+        document.documentElement.classList.add('dark');
+      } else {
+        document.documentElement.classList.remove('dark');
+      }
+    };
+
+    const hasEventListener = !!mediaQuery.addEventListener;
+
+    hasEventListener
+      ? mediaQuery.addEventListener('change', handle)
+      : mediaQuery.addListener(handle);
+    handle(mediaQuery);
+
+    return () => {
+      hasEventListener && mediaQuery.removeEventListener('change', handle);
+    };
+  };
+
+  useEffect(() => {
+    setTheme(localStorage.getItem('theme') ?? 'system');
+  }, []);
+
+  useEffect(() => {
+    if (theme === 'light') {
+      document.documentElement.classList.remove('dark');
+      localStorage.setItem('theme', theme);
+    } else if (theme === 'dark') {
+      document.documentElement.classList.add('dark');
+      localStorage.setItem('theme', theme);
+    } else if (theme === 'system') {
+      localStorage.removeItem('theme');
+      return listenDarkMode();
+    }
+  }, [theme]);
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" size="icon">
+          <SunIcon className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <MoonIcon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme('light')}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('dark')}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('system')}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
---
+++ src/components/ui/button.tsx
@@ -0,0 +1,54 @@
+import { Slot } from '@radix-ui/react-slot';
+import { cva, type VariantProps } from 'class-variance-authority';
+import * as React from 'react';
+
+import { cn } from '~/libs/utils';
+
+const buttonVariants = cva(
+  'inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-200 disabled:pointer-events-none disabled:opacity-50',
+  {
+    variants: {
+      variant: {
+        default: 'hover:bg-gray-900/90 bg-gray-900 text-gray-50',
+        destructive: 'bg-red-500 hover:bg-red-500/90 text-gray-50',
+        outline:
+          'bg-white border border-gray-200 hover:bg-gray-100 hover:text-gray-900',
+        secondary: 'hover:bg-gray-100/80 bg-gray-100 text-gray-900',
+        ghost: 'hover:bg-gray-100 hover:text-gray-900',
+        link: 'text-gray-900 underline-offset-4 hover:underline',
+      },
+      size: {
+        default: 'h-10 px-4 py-2',
+        sm: 'h-9 rounded-md px-3',
+        lg: 'h-11 rounded-md px-8',
+        icon: 'h-10 w-10',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
+    },
+  },
+);
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : 'button';
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
+);
+Button.displayName = 'Button';
+
+export { Button, buttonVariants };

6598303f419c114c5d6b49680087f8064e6bbf92
bepyan <bepyan@naver.com>
2023-12-24 22:59:27 +0900

    fix: toc 제목 클릭시 이전 제목이 하이라이트되는 버그

--- src/components/table-of-content.tsx
+++ src/components/table-of-content.tsx
@@ -63,20 +63,22 @@ const useTocScroll = (tableOfContents: TOCSection[]) => {
 
       let current: typeof currentSectionSlug = undefined;
       const top = window.scrollY + pageTop;
+      const HEADING_OFFSET = 10;
 
-      for (let i = 0; i < headings.length; i++) {
-        if (top >= headings[i].top) {
-          current = headings[i].id;
+      headings.forEach((heading) => {
+        if (top >= heading.top - HEADING_OFFSET) {
+          current = heading.id;
         }
-      }
+      });
 
       setCurrentSectionSlug(current);
-    }, 300);
+    }, 10);
 
     onResize();
     onScroll();
     window.addEventListener('scroll', onScroll, { capture: true });
     window.addEventListener('resize', onResize, { capture: true });
+
     return () => {
       window.removeEventListener('scroll', onScroll, { capture: true });
       window.removeEventListener('resize', onResize, { capture: true });

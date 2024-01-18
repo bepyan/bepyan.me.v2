6756bda36ca98c043bfee673d909859cb93c0af0
bepyan <bepyan@naver.com>
2024-01-19 00:25:20 +0900

    fix: tsc error

--- src/content/post/ko/writing/the-two-reacts.tsx
+++ src/content/post/ko/writing/the-two-reacts.tsx
@@ -12,14 +12,14 @@ export function Counter() {
   );
 }
 
-export function PostPreview({ slug, title, wordCount }) {
+export function PostPreview({ slug = '', title = '', wordCount = 0 }) {
   return (
     <section className="rounded-md bg-gray-100 px-4 py-2">
-      <h5 className="font-bold">
+      <p className="font-bold">
         <a href={`/post/${slug}`} target="_blank">
           {title}
         </a>
-      </h5>
+      </p>
       <i>{wordCount} 단어</i>
     </section>
   );

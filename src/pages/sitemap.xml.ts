943d906b30be7488722dcd20bef93cde96f69d4f
bepyan <bepyan@naver.com>
2024-02-02 13:25:09 +0900

    feat: sitemap remove hreflang

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -3,11 +3,10 @@ import { getPostInfoList } from '~/libs/mdx';
 export async function GET() {
   const siteUrl = import.meta.env.SITE;
   const posts = await getPostInfoList('all');
-  const enPosts = posts.filter((post) => post.lang === 'en');
 
   const renderUrl = (slug: string, hasEn = true) => {
     const hreflang = hasEn
-      ? `<xhtml:link rel="alternate" hreflang="ko" href="${siteUrl}${slug}" /><xhtml:link rel="alternate" hreflang="en" href="${siteUrl}/en${slug}" />`
+      ? `<xhtml:link rel="alternate" hreflang="en" href="${siteUrl}/en${slug}" />`
       : '';
 
     return `<url><loc>${siteUrl}</loc>${hreflang}</url>`;
@@ -18,19 +17,14 @@ export async function GET() {
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
     ${renderUrl('/')}
     ${renderUrl('/writing')}
-    ${renderUrl('/note')}
+    ${renderUrl('/note')} 
     ${renderUrl('/craft')}
     ${posts
-      .filter((post) => post.lang === 'ko')
       .map((post) => {
+        const langPrefix = post.lang === 'ko' ? '' : `/${post.lang}`;
         const lastMod = (post.updatedDate ?? post.date).toISOString();
-
-        const hasEn = enPosts.some((item) => item.href === post.href);
-        const hreflang = hasEn
-          ? `<xhtml:link rel="alternate" hreflang="ko" href="https://bepyan.me${post.href}" /><xhtml:link rel="alternate" hreflang="en" href="https://bepyan.me/en${post.href}" />`
-          : '';
-
-        return `<url><loc>${siteUrl}${post.href}</loc><lastmod>${lastMod}</lastmod>${hreflang}</url>`;
+        const slug = `${langPrefix}${post.href}`;
+        return `<url><loc>${siteUrl}${slug}</loc><lastmod>${lastMod}</lastmod></url>`;
       })
       .join('\n')}
   </urlset>

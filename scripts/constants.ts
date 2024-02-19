a4f9f1b105fd8ecb89d381e897d018f41b65de89
bepyan <bepyan@naver.com>
2024-02-19 20:54:40 +0900

    draft: octokit api

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -1,8 +1,13 @@
 export const GITHUB_TOKEN = process.env['GITHUB_TOKEN'];
 export const GITHUB_REPOSITORY = process.env['GITHUB_REPOSITORY']!;
-// prettier-ignore
-export const [GITHUB_REPOSITORY_NAME, GITHUB_REPOSITORY_OWNER] = GITHUB_REPOSITORY.split('/');
-export const GITHUB_ISSUE_NUMBER = +process.env['GITHUB_ISSUE_NUMBER']!;
+export const [GITHUB_REPOSITORY_NAME, GITHUB_REPOSITORY_OWNER] =
+  GITHUB_REPOSITORY.split('/');
+
+export const GITHUB_PULL_REQUEST = JSON.parse(
+  process.env['GITHUB_PULL_REQUEST']!,
+);
+export const GITHUB_ISSUE_NUMBER = GITHUB_PULL_REQUEST.number;
+export const GITHUB_RECENT_COMMIT_SHA = GITHUB_PULL_REQUEST.head.sha;
 
 if (!GITHUB_TOKEN) {
   console.log('::error:: There is no GITHUB_TOKEN environment variable');
--- scripts/github-api.ts
+++ scripts/github-api.ts
@@ -2,20 +2,62 @@ import { Octokit } from '@octokit/rest';
 
 import {
   GITHUB_ISSUE_NUMBER,
+  GITHUB_PULL_REQUEST,
+  GITHUB_RECENT_COMMIT_SHA,
   GITHUB_REPOSITORY_NAME,
   GITHUB_REPOSITORY_OWNER,
   GITHUB_TOKEN,
 } from './constants';
 
+const owner = GITHUB_REPOSITORY_OWNER;
+const repo = GITHUB_REPOSITORY_NAME;
+
 export const api = new Octokit({
-  auth: `token ${GITHUB_TOKEN}`,
+  auth: GITHUB_TOKEN,
 });
 
 export const createPRComment = (content: string) => {
   return api.rest.issues.createComment({
-    owner: GITHUB_REPOSITORY_OWNER,
-    repo: GITHUB_REPOSITORY_NAME,
+    owner,
+    repo,
     issue_number: GITHUB_ISSUE_NUMBER,
     body: content,
+  });
+};
+
+export const createCommit = async ({
+  treeBlobs,
+  message,
+}: {
+  treeBlobs: any;
+  message: string;
+}) => {
+  const latestCommit = await api.rest.git.getCommit({
+    owner,
+    repo,
+    commit_sha: GITHUB_RECENT_COMMIT_SHA,
+  });
+  const baseTree = latestCommit.data.tree.sha;
+
+  const newTree = await api.git.createTree({
+    owner,
+    repo,
+    base_tree: baseTree,
+    tree: treeBlobs,
+  });
+
+  const commit = await api.rest.git.createCommit({
+    owner,
+    repo,
+    message,
+    tree: newTree.data.sha,
+    parents: [GITHUB_RECENT_COMMIT_SHA],
+  });
+
+  await api.git.updateRef({
+    owner,
+    repo,
+    ref: `heads/${GITHUB_PULL_REQUEST.head.ref}`,
+    sha: commit.data.sha,
   });
 };

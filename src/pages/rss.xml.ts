6b06a3a8fa5b683319dd6b8a6a29632b2e8e8210
bepyan <bepyan@naver.com>
2024-02-23 19:03:51 +0900

    fix: RSS에 i18n이 반영이 안된 이슈

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,6 +1,7 @@
 import rss from '@astrojs/rss';
 
 import { SITE } from '~/consts';
+import { useTranslatedPath } from '~/i18n/utils';
 import { getPostInfoList } from '~/libs/mdx';
 
 export async function GET(context: any) {
@@ -10,10 +11,13 @@ export async function GET(context: any) {
     title: SITE.title,
     description: SITE.description,
     site: context.site,
-    items: posts.map((post) => ({
-      ...post,
-      pubDate: new Date(post.date),
-      link: post.href,
-    })),
+    items: posts.map((post) => {
+      const t = useTranslatedPath(post.lang);
+      return {
+        ...post,
+        pubDate: new Date(post.date),
+        link: t(post.href),
+      };
+    }),
   });
 }

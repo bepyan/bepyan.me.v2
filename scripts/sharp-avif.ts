7c90ef7760d7d94d576b766a0a5d1168943a95d3
bepyan <bepyan@naver.com>
2024-11-11 00:08:38 +0900

    feat(sharp): replace png in the target's directory

--- scripts/sharp-avif.ts
+++ scripts/sharp-avif.ts
@@ -3,17 +3,31 @@
 import { Glob } from 'bun';
 import sharp from 'sharp';
 
+const ignoreList = ['og1.png', 'og2.png'];
+
 (async () => {
   const glob = new Glob(`public/img/**/*.png`);
 
   for await (const filePath of glob.scan('.')) {
     const filename = filePath.split('/').pop()!;
 
-    await sharp(filePath)
-      .toFormat('avif')
-      .avif()
-      .toFile(filename + '.avif');
+    if (ignoreList.includes(filename)) {
+      continue;
+    }
 
-    // await unlink(filePath);
+    try {
+      await sharp(filePath)
+        .toFormat('avif')
+        .avif()
+        .toFile(filePath.replace('.png', '.avif'));
+
+      // await unlink(filePath);
+      // 마크다운에서 .png 부분을 .avif로 변경하는 코드 추가 필요
+
+      console.log(`✦ ${filePath} complete`);
+    } catch (e) {
+      console.error(e);
+      console.log(`✧ ${filePath} error`);
+    }
   }
-})();
+})().then(console.error);

612c9f430c490d52ce4e971f415c1b40723c6348
bepyan <bepyan@naver.com>
2024-11-13 20:48:06 +0900

    feat: shiki transformer add lang span

--- plugins/transformer-fragment.ts
+++ plugins/transformer-fragment.ts
@@ -1,6 +1,8 @@
 import type { Element } from 'hast';
 import type { ShikiTransformer } from 'shiki';
 
+const ignoreLangList = ['', 'plaintext'];
+
 export function transformerFragment(): ShikiTransformer {
   return {
     name: '@shikijs/transformers:fragment',
@@ -8,17 +10,30 @@ export function transformerFragment(): ShikiTransformer {
       const metaString = this.options.meta?.__raw || '';
       const title = metaString.match(/title="([^"]+)"/)?.[1];
       const caption = metaString.match(/caption="([^"]+)"/)?.[1];
+      const lang = this.options.lang;
 
       root.children = [
         // @ts-expect-error hast type conflict internally
         createFragmentElement(
           title && createTitleElement(title),
+          !ignoreLangList.includes(lang) && createLanguageElement(lang),
           root.children[0],
           caption && createCaptionElement(caption),
         ),
       ];
       return root;
     },
+  };
+}
+
+function createFragmentElement(...children: unknown[]): Element {
+  return {
+    type: 'element',
+    tagName: 'div',
+    properties: {
+      'data-code-fragment': '',
+    },
+    children: (children as Element[]).filter(Boolean),
   };
 }
 
@@ -44,13 +59,13 @@ function createCaptionElement(caption: string): Element {
   };
 }
 
-function createFragmentElement(...children: unknown[]): Element {
+function createLanguageElement(lang: string): Element {
   return {
     type: 'element',
-    tagName: 'div',
+    tagName: 'span',
     properties: {
-      'data-code-fragment': '',
+      class: 'lang',
     },
-    children: (children as Element[]).filter(Boolean),
+    children: [{ type: 'text', value: lang }],
   };
 }

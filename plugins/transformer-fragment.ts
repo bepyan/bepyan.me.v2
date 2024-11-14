1dba586d589048c469fe1965a6786d47f41ede1c
bepyan <bepyan@naver.com>
2024-11-14 23:34:14 +0900

    feat: copy button

--- plugins/transformer-fragment.ts
+++ plugins/transformer-fragment.ts
@@ -10,14 +10,18 @@ export function transformerFragment(): ShikiTransformer {
       const metaString = this.options.meta?.__raw || '';
       const title = metaString.match(/title="([^"]+)"/)?.[1];
       const caption = metaString.match(/caption="([^"]+)"/)?.[1];
+      const noCopy = metaString.includes('noCopy');
       const lang = this.options.lang;
 
       root.children = [
         // @ts-expect-error hast type conflict internally
         createFragmentElement(
           title && createTitleElement(title),
-          !ignoreLangList.includes(lang) && createLanguageElement(lang),
-          root.children[0],
+          createCodeWrapperElement(
+            !ignoreLangList.includes(lang) && createLanguageElement(lang),
+            !noCopy && createCopyElement(),
+            root.children[0],
+          ),
           caption && createCaptionElement(caption),
         ),
       ];
@@ -59,6 +63,17 @@ function createCaptionElement(caption: string): Element {
   };
 }
 
+function createCodeWrapperElement(...children: unknown[]): Element {
+  return {
+    type: 'element',
+    tagName: 'div',
+    properties: {
+      'data-code-wrapper': '',
+    },
+    children: (children as Element[]).filter(Boolean),
+  };
+}
+
 function createLanguageElement(lang: string): Element {
   return {
     type: 'element',
@@ -67,5 +82,17 @@ function createLanguageElement(lang: string): Element {
       class: 'lang',
     },
     children: [{ type: 'text', value: lang }],
+  };
+}
+
+function createCopyElement() {
+  return {
+    type: 'element',
+    tagName: 'button',
+    properties: {
+      class: 'copy',
+      title: 'Copy Code',
+    },
+    children: [],
   };
 }

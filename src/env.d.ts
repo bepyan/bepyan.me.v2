7e78c1424c15ccb24c5ab62250ccce87d776b401
bepyan <bepyan@naver.com>
2025-04-03 18:44:54 +0900

    remove ga

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,12 +1,3 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-
-interface ImportMetaEnv {
-  readonly PUBLIC_GA_ID: string;
-  readonly PUBLIC_MIXPANEL_ID: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}

36f55e98d00190b6b259d8e0339675aaa4942b16
bepyan <bepyan@naver.com>
2025-04-03 18:31:27 +0900

    feat: add sitemap i18n

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,14 +1,57 @@
-import { getPostInfoList } from '~/libs/mdx';
+import { format } from 'date-fns';
+
+import { getPostInfoList, type PostInfo } from '~/libs/mdx';
 
 export async function GET() {
   const siteUrl = import.meta.env.SITE;
   const posts = await getPostInfoList('all');
 
-  const renderUrl = (slug: string, hasEn = true) => {
-    let result = `<url><loc>${siteUrl}${slug}</loc></url>`;
+  const [koPosts, enPosts] = posts.reduce<[PostInfo[], PostInfo[]]>(
+    ([ko, en], post) => {
+      if (post.lang === 'ko') {
+        ko.push(post);
+      } else {
+        en.push(post);
+      }
+      return [ko, en];
+    },
+    [[], []],
+  );
+
+  const renderUrl = (
+    slug: string,
+    {
+      hasEn = true,
+      lastMod,
+    }: {
+      hasEn?: boolean;
+      lastMod?: string;
+    } = {},
+  ) => {
+    const enSlug = `/en${slug}`;
+    let result = '';
+
+    result += '<url>';
+    result += `<loc>${siteUrl}${slug}</loc>`;
+    if (lastMod) {
+      result += `<lastmod>${lastMod}</lastmod>`;
+    }
+    result += `<xhtml:link rel="alternate" hreflang="ko" href="${siteUrl}${slug}"/>`;
+    if (hasEn) {
+      result += `<xhtml:link rel="alternate" hreflang="en" href="${siteUrl}${enSlug}"/>`;
+    }
+    result += `<xhtml:link rel="alternate" hreflang="x-default" href="${siteUrl}${slug}"/>`;
+    result += '</url>';
 
     if (hasEn) {
-      result += `<url><loc>${siteUrl}/en${slug}</loc></url>`;
+      result += '<url>';
+      result += `<loc>${siteUrl}${enSlug}</loc>`;
+      if (lastMod) {
+        result += `<lastmod>${lastMod}</lastmod>`;
+      }
+      result += `<xhtml:link rel="alternate" hreflang="en" href="${siteUrl}${enSlug}"/>`;
+      result += `<xhtml:link rel="alternate" hreflang="ko" href="${siteUrl}${slug}"/>`;
+      result += '</url>';
     }
 
     return result;
@@ -16,17 +59,17 @@ export async function GET() {
 
   const result = `
   <?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
     ${renderUrl('/')}
     ${renderUrl('/writing')}
     ${renderUrl('/note')} 
     ${renderUrl('/craft')}
-    ${posts
+    ${koPosts
       .map((post) => {
-        const langPrefix = post.lang === 'ko' ? '' : `/${post.lang}`;
-        const lastMod = (post.updatedDate ?? post.date).toISOString();
-        const slug = `${langPrefix}${post.href}`;
-        return `<url><loc>${siteUrl}${slug}/</loc><lastmod>${lastMod}</lastmod></url>`;
+        const lastMod = format(post.updatedDate ?? post.date, 'yyyy-MM-dd');
+        const hasEn = enPosts.some((p) => p.href === post.href);
+
+        return renderUrl(post.href, { hasEn, lastMod });
       })
       .join('\n')}
   </urlset>
